656bf128a2420492408a1bd60423d449caae4d89
ansidev <ansidev@gmail.com>
2014-11-08 16:22:14 +0700

    Fix config for 'jsBaseUrl'

--- lib/Cake/bootstrap.php
+++ lib/Cake/bootstrap.php
@@ -183,7 +183,7 @@ if (!defined('FULL_BASE_URL')) {
 Configure::write('App.imageBaseUrl', IMAGES_URL);
 Configure::write('App.cssBaseUrl', CSS_URL);
 Configure::write('App.fontsBaseUrl', FONTS_URL);
-Configure::write('App.cssBaseUrl', CSS_URL);
+Configure::write('App.jsBaseUrl', JS_URL);
 
 App::$bootstrapping = true;
 
