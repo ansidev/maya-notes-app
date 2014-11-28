5542f92ab34549211750ec180a73ea9a9c9e1b97
ansidev <ansidev@gmail.com>
2014-11-28 18:19:54 +0700

    Edit routing url

--- app/Config/routes.php
+++ app/Config/routes.php
@@ -30,7 +30,7 @@
  */
 	Router::connect('/pages/*', array('controller' => 'pages', 'action' => 'display'));
 
-	Router::connect('/users/notes', array('controller' => 'users', 'action' => 'index'));
+	Router::connect('/user/notes', array('controller' => 'notes', 'action' => 'index'));
 
 /**
  * Load all plugin routes. See the CakePlugin documentation on
