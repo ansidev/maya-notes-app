ceffb28efb35a567c34b30ed31f06e6bd7704436
ansidev <ansidev@gmail.com>
2014-12-04 15:29:52 +0700

    Define $paginate

--- app/Controller/AppController.php
+++ app/Controller/AppController.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * Application level Controller
  *
@@ -18,7 +19,6 @@
  * @since         CakePHP(tm) v 0.2.9
  * @license       http://www.opensource.org/licenses/mit-license.php MIT License
  */
-
 App::uses('Controller', 'Controller');
 
 /**
@@ -31,112 +31,130 @@ App::uses('Controller', 'Controller');
  * @link		http://book.cakephp.org/2.0/en/controllers.html#the-app-controller
  */
 class AppController extends Controller {
-	public $components = array(
-		'DebugKit.Toolbar',
-		'Session',
-		// 'Security',
-		'Auth' => array(
-			'loginAction' => array(
-				'controller' => 'users',
-				'action' => 'login'
-			),
-			'loginRedirect'	=> array(
-				'controller' => 'notes',
-				'action' => 'index'
-			),
-			'loginError' => 'Invalid username or password. Please try again!',
-			'logoutRedirect' => array(
-				'controller' => '/',
-				'action' => '',
-			),
-			'authenticate' => array(
-				'Form' => array(
+
+    public $components = array(
+        'DebugKit.Toolbar',
+        'Session',
+        // 'Security',
+        'Auth' => array(
+            'loginAction' => array(
+                'controller' => 'users',
+                'action' => 'login'
+            ),
+            'loginRedirect' => array(
+                'controller' => 'notes',
+                'action' => 'index'
+            ),
+            'loginError' => 'Invalid username or password. Please try again!',
+            'logoutRedirect' => array(
+                'controller' => '/',
+                'action' => '',
+            ),
+            'authenticate' => array(
+                'Form' => array(
                     'userModel' => 'User',
                     'passwordHasher' => 'Blowfish',
-					'fields' => array(
-						'username' => 'user_name',
-						'password' => 'user_pass'
-					),
-				)
-			),
-			'authorize' => array('Controller')
-		),
-	);
-	public $helpers = array('Form','Html','Session');
+                    'fields' => array(
+                        'username' => 'user_name',
+                        'password' => 'user_pass'
+                    ),
+                )
+            ),
+            'authorize' => array('Controller')
+        ),
+    );
+    public $helpers = array('Form', 'Html', 'Session');
+    public $paginate = array(
+        'Notebook' => array(
+            'limit' => 10,
+            'order' => array(
+                'Notebook.user_id' => 'asc',
+                'Notebook.id' => 'asc',
+                'Notebook.book_name' => 'asc'
+            ),
+        ),
+        'Note' => array(
+            'limit' => 10,
+            'order' => array(
+                'Note.user_id' => 'asc',
+                'Note.id' => 'asc',
+                'Note.note_title' => 'asc'
+            ),
+        ),
+    );
 
+    public function beforeFilter() {
+        // Cache::clear();
+        $this->Auth->allow('display');
+        Security::setHash('blowfish');
+        $this->set('is_admin', $this->isAdmin());
+        $this->set('is_logged_in', $this->isLoggedIn());
+        $this->set('users_id', $this->getUsersId());
+        $this->set('users_user_name', $this->getUsersUserLogin());
+        $this->set('users_display_name', $this->getUsersDisplayName());
+    }
 
-	public function beforeFilter() {
-		// Cache::clear();
-		$this->Auth->allow('display');
-		Security::setHash('blowfish');
-		$this->set('is_admin', $this->isAdmin());
-		$this->set('is_logged_in', $this->isLoggedIn());
-		$this->set('users_id', $this->getUsersId());
-		$this->set('users_user_name', $this->getUsersUserLogin());
-		$this->set('users_display_name', $this->getUsersDisplayName());
-	}
+    function beforeRender() {
+        if ($this->Auth->login()) {
+            $this->set('session', $this->Session);
+        }
+    }
 
-	function beforeRender() {
-		// if($this->Auth->login()) {
-		// 	$this->set('session', $this->Session);
-		// }
-	}
+    //Check if current user is admin or not
+    public function isAdmin() {
+        if ($this->Auth->user('user_role') === 'admin') {
+            return true;
+        }
+        return false;
+    }
 
-	//Check if current user is admin or not
-	public function isAdmin() {
-		if($this->Auth->user('user_role') === 'admin') {
-			return true;
-		}
-		return false;
-	}
+    //Check if user is logged in or not
+    public function isLoggedIn() {
+        if ($this->Auth->user()) {
+            // $this->User->set('is_online', true);
+            return true;
+        }
+        return false;
+    }
 
-	//Check if user is logged in or not
-	public function isLoggedIn() {
-		if($this->Auth->user()) {
-			// $this->User->set('is_online', true);
-			return true;
-		}
-		return false;
-	}
+    //Get logged in user 's 'id' field in table users
+    public function getUsersId() {
+        if ($this->Auth->user()) {
+            return $this->Auth->user('id');
+        }
+        return null;
+    }
 
-	//Get logged in user 's 'id' field in table users
-	public function getUsersId() {
-		if($this->Auth->user()) {
-			return $this->Auth->user('id');
-		}
-		return null;
-	}
+    //Get logged in user 's 'user_name' field in table users
+    public function getUsersUserLogin() {
+        if ($this->Auth->user()) {
+            return $this->Auth->user('user_name');
+        }
+        return null;
+    }
 
-	//Get logged in user 's 'user_name' field in table users
-	public function getUsersUserLogin() {
-		if($this->Auth->user()) {
-			return $this->Auth->user('user_name');
-		}
-		return null;
-	}
+    //Get logged in user 's 'display_name' field in table users
+    public function getUsersDisplayName() {
+        if ($this->Auth->user()) {
+            if (!empty($this->Auth->user('display_name'))) {
+                return $this->Auth->user('display_name');
+            }
+            return $this->Auth->user('user_name');
+        }
+        return null;
+    }
 
-	//Get logged in user 's 'display_name' field in table users
-	public function getUsersDisplayName() {
-		if($this->Auth->user()) {
-			if(!empty($this->Auth->user('display_name'))) {
-				return $this->Auth->user('display_name');
-			}
-			return $this->Auth->user('user_name');
-		}
-		return null;
-	}
+    public function isAuthorized() {
+        //Any registered user can access public functions
+        if ($this->Auth->user()) {
+            return true;
+        }
+        // //Only admin can access admin function
+        // if(isset($this->request->params['admin'])) {
+        // 	return (bool)($user['user_role'] === 'admin');
+        // }
+        //Default deny
+        return false;
+    }
 
-
-	public function isAuthorized() {
-		//Any registered user can access public functions
-		if($this->Auth->user()) {
-			return true;
-		}
-		// //Only admin can access admin function
-		// if(isset($this->request->params['admin'])) {
-		// 	return (bool)($user['user_role'] === 'admin');
-		// }
-		//Default deny
-		return false;
-	}
 }
